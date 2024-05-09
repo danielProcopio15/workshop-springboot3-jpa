d3bb684d380452016f69ae7f2b6c7a2795ceb54a
danielProcopio15 <danielprocopio15@gmail.com>
2024-05-09 17:54:07 -0300

    migrando de IDE

--- src/main/java/com/javaguides/course/entities/User.java
+++ src/main/java/com/javaguides/course/entities/User.java
@@ -1,15 +1,20 @@
 package com.javaguides.course.entities;
 import jakarta.persistence.*;
-
 import java.io.Serializable;
 import java.util.Objects;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
 
 
 
 
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable {
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
