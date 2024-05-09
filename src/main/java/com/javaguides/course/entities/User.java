5b5e4409a3a922fb5ea0f30025c778000eed4190
danielProcopio15 <danielprocopio15@gmail.com>
2024-05-09 16:45:42 -0300

    H2 database, test profile, JPA

--- src/main/java/com/javaguides/course/entities/User.java
+++ src/main/java/com/javaguides/course/entities/User.java
@@ -1,10 +1,18 @@
 package com.javaguides.course.entities;
+import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.util.Objects;
 
+
+
+
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String email;
